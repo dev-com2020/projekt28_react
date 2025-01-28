4d27ac0a84166bbbafd883a3b76f2fd740c277ec
DEV-COM <kwp@tlen.pl>
2025-01-28 12:03:21 +0100

    r1

--- src/App.jsx
+++ src/App.jsx
@@ -21,10 +21,29 @@ function App() {
         }
     ];
     const [posts, setPosts] = useState(initialPosts)
-
+    const [postContent, setPostContent] = useState('')
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        const NewPost = {
+            id: posts.length + 1,
+            text: postContent,
+            user: {
+                avatar: 'https://avatars1.githubusercontent.com/u/55',
+                username: 'John',
+            }
+        }
+        setPosts([NewPost, ...posts])
+        setPostContent('')
+    }
 
     return (
         <>
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <textarea value={postContent} onChange={(e) => setPostContent(e.target.value)}/>
+                    <input type="submit" value="Submit"/>
+                </form>
+            </div>
             <div>
                 {posts.map((post) =>
                     <div key={post.id}>
