1921c70d9eb2bf41637a1f4b511fe48ba95a42d7
DEV-COM <kwp@tlen.pl>
2025-01-29 15:36:40 +0100

    r12

--- src/server/services/graphql/resolvers.js
+++ src/server/services/graphql/resolvers.js
@@ -8,21 +8,31 @@ export default function resolvers() {
     const resolvers = {
         RootQuery: {
             posts(root, args, context) {
-                return Post.findAll({})
-            },
+                return Post.findAll({
+                    order: [
+                        ['createdAt', 'DESC']
+                    ]
+                });
+            }
         },
-        RootMutation: {
-            addPost(root, {post, user}, context) {
-                const postObject = {
-                    ...post,
-                    user,
-                    id: posts.length + 1
-                }
-                posts.push(postObject)
-                logger.log({level: 'info', message: `Added ${posts.length} posts`})
-                return postObject
+            Post: {
+                user(root, args, context) {
+                    return post.getUser();
+                },
             },
-        },
-    };
+            RootMutation: {
+                addPost(root, {post, user}, context) {
+                    const postObject = {
+                        ...post,
+                        user,
+                        id: posts.length + 1
+                    }
+                    posts.push(postObject)
+                    logger.log({level: 'info', message: `Added ${posts.length} posts`})
+                    return postObject
+                },
+            },
+        };
+
     return resolvers;
 }
